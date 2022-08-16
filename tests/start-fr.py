95b46fcc68622f220a8cca508bbfe44696c9a19f
RajivQA <MY_NAME@example.com>
2022-08-16 15:18:05 +0530

    Initial commit [demo commmit ]

---
+++ tests/start-fr.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.implicitly_wait(time_to_wait=100)
+driver.get("https://www.udemy.com/course/selenium-with-python/learn/lecture/12805019#overview")
+driver.get("https://www.youtube.com/")
+
+driver.quit()
