b97d4c544dd2946879bf0192db1a9d81184c6045
RajivQA <MY_NAME@example.com>
2022-08-16 15:20:20 +0530

    first commit

--- tests/start-fr.py
+++ tests/start-fr.py
@@ -5,5 +5,6 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(time_to_wait=100)
 driver.get("https://www.udemy.com/course/selenium-with-python/learn/lecture/12805019#overview")
 driver.get("https://www.youtube.com/")
-
+driver.maximize_window()
+driver.get('https://www.google.com')
 driver.quit()
